3f198ed8f91adc13d3fa26a0cc35c3f616f6d3b1
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-08 22:52:51 +0530

    removes openai halfdone functions altogether.

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,4 +1,2 @@
-import { OpenAI } from "openai";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-export const openai = new OpenAI({ "apiKey": process.env.OPEN_API_KEY });
 export const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
